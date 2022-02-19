147f1390d4c5397f09629cb9862788be9435b9c9
Wil <molina.wilfredo.j@gmail.com>
2022-02-18 21:17:00 -0600

    debugging

--- lambdas/esxlambdatickers/lambda_function.py
+++ lambdas/esxlambdatickers/lambda_function.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 import urllib3
 from cachetools.func import TTLCache, ttl_cache
 
-TIME_DELTA_CLIENT = timedelta(seconds=1)
-TIME_DELTA_TICKER = timedelta(seconds=1)
+TIME_DELTA_CLIENT = timedelta(seconds=5)
+TIME_DELTA_TICKER = timedelta(seconds=5)
 MAX_TICKERS = 9
 MAX_SIZE_CLIENT_CACHE = 100
 MAX_SIZE_TICKER_CACHE = MAX_SIZE_CLIENT_CACHE * MAX_TICKERS
@@ -16,12 +16,13 @@ API_KEY = os.environ.get("API_KEY")
 @ttl_cache(ttl=TIME_DELTA_TICKER, timer=datetime.now, maxsize=MAX_SIZE_TICKER_CACHE)
 def get_ticker(ticker):
     http = urllib3.PoolManager()
-    return json.loads(
+    response = json.loads(
         http.request(
             "GET",
             f"https://financialmodelingprep.com/api/v3/profile/{ticker}?apikey={API_KEY}",
         ).data.decode("utf8")
-    )[0]
+    )
+    return response[0] if response else {}
 
 
 def get_clientip(
